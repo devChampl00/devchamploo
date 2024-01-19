54c1e47fb54814cff992a465e2c68f7323ff81b1
Umam Alfarizi <alfariziuchiha@gmail.com>
2024-01-19 09:36:52 +0800

    tes

--- app.js
+++ app.js
@@ -34,12 +34,16 @@ app.use(
   })
 )
 
-app.use(express.static('public'))
-
 // Templating engine
 app.use(expressLayout)
 app.set('layout', './layouts/main')
+
+// app.set('view engine', 'ejs')
+// app.use(express.static('public'))
+
+app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
+app.use(express.static(__dirname + 'public'))
 
 // app.use(uploadFile, checkFileType)
 app.use(uploadFile)
